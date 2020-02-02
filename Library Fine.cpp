60b806865865a9d8e5cc1805089eb9d1f6430466
Priyavadhani <42804642+priya-tech@users.noreply.github.com>
2020-02-02 09:49:04 +0530

    Add files via upload

---
+++ Library Fine.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int d1,m1,y1,d2,m2,y2,fine;
+    cin>>d1>>m1>>y1;
+    cin>>d2>>m2>>y2;
+    if(y1==y2){
+        if(m1==m2){
+            if(d1>d2){
+                fine=15*(d1-d2);
+            }
+            else{
+                fine=0;
+            }
+        }
+        else if(m1<m2){
+            fine=0;
+        }
+        else{
+            fine=500*(m1-m2);
+        }
+    }
+    else if(y1<y2){
+        fine=0;
+    }
+    else{
+        fine=10000;
+    }
+    cout<<fine;
+    return 0;
+}
+
